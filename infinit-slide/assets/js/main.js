dfd5be23dcefba4a6ecdead5c97be2111d682d52
kesav424 <kesav424@gmail.com>
2023-11-29 08:57:51 +0530

    resolve bug

--- infinit-slide/assets/js/main.js
+++ infinit-slide/assets/js/main.js
@@ -11,11 +11,13 @@ next.addEventListener("click", () => {
 });
 
 prev.addEventListener("click", () => {
+  dirction = 1;
   if (dirction == -1) {
+    dirction = 1;
     slide.appendChild(slide.firstElementChild);
   }
-  dirction = 1;
-  //   curosoul.style.justifyContent = "flex-end";
+
+  // curosoul.style.justifyContent = "flex-end";
   slide.style.transform = "translateX(20%)";
 });
 
